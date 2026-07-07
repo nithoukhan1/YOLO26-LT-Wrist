de9996333cb43fad14a31775b1db5cc52918f0d9
nithoukhan1 <inayatdms@yahoo.com>
2026-07-07 10:31:19 +0800

    syntax error

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -8,8 +8,7 @@ from copy import deepcopy
 from pathlib import Path
 
 import torch
-import torch.nn as 
-# Import it at the top of tasks.py or inside parse_model
+import torch.nn as nn
 from ultralytics.nn.modules import VSSBlock
 
 from ultralytics.nn.autobackend import check_class_names
